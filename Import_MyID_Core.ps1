4a09a021624c455df4e1d6ab6ebab261a6a99e2c
Shiraz Esat <shiraz.esat@gmail.com>
2023-02-09 10:44:21 +0000

    Perform exception handling within functions
    try/catch blocks now within API calling functions
    all variables can now be passed as parameters
    consistent camelCase variable naming

--- Import_MyID_Core.ps1
+++ Import_MyID_Core.ps1
@@ -4,23 +4,34 @@
 
 Param
 (
-    [string]$auth_url = "https://react.domain31.local/web.oauth2/connect/token",
-    [string]$api_url = "https://react.domain31.local/rest.core/api",
+    [string]$authUrl = "https://react.domain31.local/web.oauth2/connect/token",
+    [string]$apiUrl = "https://react.domain31.local/rest.core/api",
     [string]$clientId = "client id",
     [string]$clientSecret = "client secret",
-    [string]$csv_location = "C:\temp\ACastle2.csv"
-
+    [string]$groupName = "Technology",
+    [string]$roleName = "MyID_PROD_Cardholders",
+    [string]$roleScope = "self",
+    [string]$domain = "domain31",
+    [string]$credProfileName = "TMO_1"
 )
 
 Function Invoke-CoreAPI-Get {
     Param
     (
         [Parameter(Mandatory)]
-        [string] $Location
+        [string] $Location,
+        [string] $FailureMessage = ""
     )
 
-    $apiResponse = Invoke-WebRequest -Uri "$api_url/$Location" -Headers $apiHeaders
-    return ConvertFrom-Json $apiResponse.Content
+    try {
+        $apiResponse = Invoke-WebRequest -Uri "$apiUrl/$Location" -Headers $apiHeaders
+        return ConvertFrom-Json $apiResponse.Content
+    }
+    catch {
+        Write-Host "ERROR - $FailureMessage. $_"
+        return ""
+    }
+
 }
 
 Function Invoke-CoreAPI-Post {
@@ -28,23 +39,29 @@ Function Invoke-CoreAPI-Post {
     (
         [Parameter(Mandatory)]
         [string] $Location,
+        [string] $FailureMessage = "",
 
         [Parameter(Mandatory)]
         [object] $Body
     )
 
-    $apiResponse = Invoke-WebRequest -Uri "$api_url/$Location" -Headers $apiHeaders -Method Post -Body ($Body | ConvertTo-Json)
-    return ConvertFrom-Json $apiResponse.Content
+    try {
+        $apiResponse = Invoke-WebRequest -Uri "$apiUrl/$Location" -Headers $apiHeaders -Method Post -Body ($Body | ConvertTo-Json)
+        return ConvertFrom-Json $apiResponse.Content
+    }
+    catch {
+        Write-Host "ERROR - $FailureMessage. $_"
+        return ""
+    }
 }
 
 ################ Auth
-
 $headers = @{
     'Authorization' = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes("${clientId}:${clientSecret}"))
 }
 
 try {
-    $tokenResponse = Invoke-WebRequest -Uri $auth_url -Method Post -Headers $headers  -Body "grant_type=client_credentials"
+    $tokenResponse = Invoke-WebRequest -Uri $authUrl -Method Post -Headers $headers  -Body "grant_type=client_credentials"
     $tokenResponseJSON = ConvertFrom-Json $tokenResponse.Content
 
     $token = $tokenResponseJSON.access_token 
@@ -60,17 +77,9 @@ $apiHeaders = @{
 }
 
 ################ Get Group
-$groupName = "Technology"
-
-try {
-    $groupId = (Invoke-CoreAPI-Get -Location "groups?q=$groupName").results.id
-}
-catch {
-    return "ERROR - Unable to get group. $_"
-}
-
+$groupId = (Invoke-CoreAPI-Get -Location "groups?q=$groupName" -FailureMessage "Unable to get group").results.id
 if (!$groupId) {
-    return "Group '$groupName' doesn't exist"
+    return "Unable to find Group '$groupName'"
     # We could add group through API, but it seems more sensible that Config should be done ahead
     # Similar to the roles we expect the system to have
 }
@@ -78,18 +87,9 @@ if (!$groupId) {
 ################ Get Role
 # Make sure role we are trying to set exists
 # Unfortunately this API endpoint doesn't filter on name
-$role = "MyID_PROD_Cardholders"
-$scope = "self"
-
-try {
-    $roleId = (Invoke-CoreAPI-Get -Location "roles").results | Where-Object id -eq $role
-}
-catch {
-    return "ERROR - Unable to get role. $_"
-}
-
+$roleId = (Invoke-CoreAPI-Get -Location "roles" -FailureMessage "Unable to get role").results | Where-Object id -eq $roleName
 if (!$roleId) {
-    return "Role '$role' doesn't exist"
+    return "Unable to find Role '$roleName'"
 }
 
 # note objectGUID/uniqueID from LDAP can't be used
@@ -97,11 +97,7 @@ if (!$roleId) {
 #                            <UniqueID>'+$UniqueID+'</UniqueID>
 
 ################ Add User
-
-$domain = "domain31"
-
 Import-CSV -Path .\ACastle2.csv -Delimiter ';' |
-
 ForEach-Object {
     $body = @{
         name      = @{
@@ -116,9 +112,9 @@ ForEach-Object {
         }
         roles     = @(
             @{
-                id    = $role
-                name  = $role
-                scope = $scope
+                id    = $roleName
+                name  = $roleName
+                scope = $roleScope
             }
         )
         account   = @{
@@ -145,27 +141,17 @@ ForEach-Object {
 #            <DataType>CMSRequestCard</DataType>
 
 
-try {
-    $newUser = (Invoke-CoreAPI-Post -Location "people" -Body $body).id
-}
-catch {
-    return "ERROR - Unable to add user. $_"
+$newUser = (Invoke-CoreAPI-Post -Location "people" -FailureMessage "Unable to add user" -Body $body).id
+if (!$newUser) {
     ## I don't want to accidentally add request for a different user in the system
     # We could have searched for Logon Name prior to add to avoid 'logonName already exists' errors
+    return;
 }
 
 ################ Create request
-$credProfileName = "TMO_1"
-
-try {
-    $credProfileId = (Invoke-CoreAPI-Get -Location "credprofiles?q=$credProfileName").results.id
-}
-catch {
-    return "ERROR - Unable to get credentialprofile. $_"
-}
-
+$credProfileId = (Invoke-CoreAPI-Get -Location "credprofiles?q=$credProfileName" -FailureMessage "Unable to get credential profile").results.id
 if (!$credProfileId) {
-    return "Credential profile '$credProfileName' doesn't exist"
+    return "Unable to find Credential profile '$credProfileName'"
 }
 
 $body = @{
@@ -174,12 +160,8 @@ $body = @{
     }
 }
 
-try {
-    $response = Invoke-CoreAPI-Post -Location "people/$newUser/requests" -Body $body
+$response = Invoke-CoreAPI-Post -Location "people/$newUser/requests" -FailureMessage "Unable to create request for user" -Body $body
+if ($response) {
+    "Request created for user."
+    $response
 }
-catch {
-    return "ERROR - Unable to create request for user. $_"
-}
-
-"Request created for user."
-$response
